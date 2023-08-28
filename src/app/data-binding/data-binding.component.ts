6cb158fdeac49ef682dba8ed15de1a0b3e3ca086
Rodrigo <rss.999999999@gmail.com>
2023-08-28 15:51:39 -0300

    #09: Property Binding e Interpolação

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding.component.scss']
 })
 export class DataBindingComponent {
+  url = 'http://rodrigo.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'https://www.meutimao.com.br/fotos-do-corinthians/w941/2023/08/23/yuri_alberto_lamenta_chance_perdida_no_jogo_3f7w.jpg';
+  getValor(){
+    return 1
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
 
 }
