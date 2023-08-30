d6556a88928f48b4c595fab9b04d43fe6d551b22
Rodrigo <rss.999999999@gmail.com>
2023-08-30 20:32:08 -0300

    #16: Acesso ao DOM e ao Template com ViewChild

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -16,18 +16,26 @@ export class OutputPropertyComponent {
   @ViewChild('campoInput') campoValorInput: ElementRef | undefined;
 
   incrementa() {
+    // Verifica se campoValorInput e campoValorInput.nativeElement são definidos
     if (this.campoValorInput && this.campoValorInput.nativeElement) {
+      // Obtém o valor atual do campo de input
       const value = this.campoValorInput.nativeElement.value;
+      // Incrementa o valor em 1
       this.campoValorInput.nativeElement.value = +value + 1;
     }
+    // Emite um evento mudouValor com o novo valor
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa() {
+    // Verifica se campoValorInput e campoValorInput.nativeElement são definidos
     if (this.campoValorInput && this.campoValorInput.nativeElement) {
+      // Obtém o valor atual do campo de input
       const value = this.campoValorInput.nativeElement.value;
+      // Decrementa o valor em 1
       this.campoValorInput.nativeElement.value = +value - 1;
     }
+    // Emite um evento mudouValor com o novo valor
     this.mudouValor.emit({ novoValor: this.valor });
   }
 }
